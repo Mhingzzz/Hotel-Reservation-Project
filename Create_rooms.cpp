acf21386c50da365a6aeeeef9f71dd462f7cdc88
mm <nathaphong_p@cmu.ac.th>
2022-01-20 11:32:54 +0700

    คร่าวๆ

--- Create_rooms.cpp
+++ Create_rooms.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 
 //อันนี้คือโค้ดสำหรับสร้างห้อง
-
+int numb;
 void ShowRoom(){
     cout << "No.    Room Type                  Price" <<endl;
     cout << "1.     Standard Room               1200" <<endl;
     cout << "2.     Superior Room               1500" <<endl;
     cout << "3.     Deluxe Room                 2000" <<endl;
     cout << "4.     Suite Room                  3500" <<endl;
-
-    
+    cout << "Please enter the room number that you are interested in: "; 
+    cin >> numb;
 }
 
+
+
 int main(){
-    int Inday, Outday, month;
+    
     ShowRoom();
     
 }
--- ReviewInSide.cpp
+++ ReviewInSide.cpp
@@ -1,4 +1,64 @@
 #include<iostream>
 using namespace std;
 
+int numb;
 //รีวิวภายในห้อง
+void Review(int i){
+    if (numb == 1)
+    {
+        cout <<"Standard Room" << endl <<endl;
+        cout << "   Two twin bed" << endl;
+        cout << "   Size Room 15 square meters" << endl;
+        cout << "   Air condition" <<endl;
+        cout << "   Coffee machine" <<endl;
+        cout << "   Free WiFi" << endl;
+        cout << "   Desk" <<endl;
+    }
+    if (numb == 2)
+    {
+        cout << "Superior Room" <<endl << endl;
+        cout << "   Size Room 20 square meters" <<endl;
+        cout << "   Two twin bed" << endl;
+        cout << "   Air condition" <<endl;
+        cout << "   Coffee machine" <<endl;
+        cout << "   Free WiFi" << endl;
+        cout << "   Refrigerator" <<endl;
+        cout << "   Safe" <<endl;
+        cout << "   Desk" <<endl;
+    }
+    if (numb == 3)
+    {
+        cout << "Deluxe Room" <<endl << endl;
+        cout << "   Size Room 30 square meters" <<endl;
+        cout << "   Two twin bed" << endl;
+        cout << "   Air condition" <<endl;
+        cout << "   Coffee machine" <<endl;
+        cout << "   Free WiFi" << endl;
+        cout << "   Refrigerator" <<endl;
+        cout << "   Safe" <<endl;
+        cout << "   Desk" <<endl; 
+        cout << "   Microwave" <<endl;
+    }
+    if (numb == 4)
+    {
+        cout << "Suite Room" <<endl << endl;
+        cout << "   Size Room 46 square meters" <<endl;
+        cout << "   Two twin bed" << endl;
+        cout << "   Air condition" <<endl;
+        cout << "   Coffee machine" <<endl;
+        cout << "   Free WiFi" << endl;
+        cout << "   Refrigerator" <<endl;
+        cout << "   Safe" <<endl;
+        cout << "   Desk" <<endl;
+        cout << "   Balcony" <<endl;
+        cout << "   Bathtub" <<endl;
+        cout << "   Microwave" <<endl;
+        cout << "   Kitchen room" <<endl;
+    }
+    
+    
+}
+
+int main(){
+    Review(numb);
+}
