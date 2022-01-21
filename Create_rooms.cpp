c25c48a717a05d50ecbb8fc83e32bc08a724a17c
mm <nathaphong_p@cmu.ac.th>
2022-01-21 22:12:36 +0700

    แก้Function BacktoShowRoom ให้ดูดีขึ้น

--- Create_rooms.cpp
+++ Create_rooms.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 #include<string>
+#include<algorithm>
 using namespace std;
+
 
 //อันนี้คือโค้ดสำหรับสร้างห้อง
 int numb;
+string text;
+void Review(int numb);
 void ShowRoom(){
     cout << "No.    Room Type                  Price(bath/day)" <<endl;
     cout << "1.     Standard Room               1200" <<endl;
@@ -14,13 +18,36 @@ void ShowRoom(){
 }
 
 void BackToShowRoom(){
-    cout << "\nIf you want to go back to main menu press 0:";
-    cin >> numb;
+    cout << "If you want to go back to main menu enter 'Back': ";
+    cin >> text;
+    transform(text.begin(), text.end(), text.begin(), ::tolower);
+    if (text == "back")
+    {
+        system("cls");
+        ShowRoom();
+        cin >> numb;
+        Review(numb);
+        
+    }
+    
+}
+
+void ChooseRoom(){
+    
+    cout << "\nIf you want to choose this room enter 'Next' : ";
+    transform(text.begin(), text.end(), text.begin(), ::tolower);
+    cin >> text;
+    if (text == "next")
+    {
+        
+    }
+    
 }
 
 
 int main(){
     
     ShowRoom();
+    BackToShowRoom();
     
 }
