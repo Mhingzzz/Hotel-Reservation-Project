c64d44eff86a4bd1e09f58b60ddbfb0c576288d1
nchanadee <nontapan_chanadee@cmu.ac.th>
2022-01-22 17:05:25 +0700

    add RandomDiscount

--- Main.cpp
+++ Main.cpp
@@ -12,6 +12,7 @@ void BackToShowRoom(); //กลับหน้าประเภทห้อง
 void Review(); //โชว์รายละเอียดของห้อง
 void Days(int &month, int &day); //ป้อนค่าวันเดือน
 int TotalDays(int day1,int month1,int day2,int month2); //คำนวนวันทั้งหมดที่ต้องการพัก
+void RandomDiscount(int price,int month); //function สุ่มส่วนลด รับ input เป็นราคารวมและเดือนที่จอง(วันแรก)
 
 int main(){
     ShowRoom();
@@ -138,3 +139,50 @@ int TotalDays(int day1,int month1,int day2,int month2){
     }
     
 }
+
+void RandomDiscount(int price,int month){ //function สุ่มส่วนลด รับ input เป็นราคารวมและเดือนที่จอง(วันแรก)
+
+    int decision; 
+    while(decision < 1 || decision > 2){ //ตัวเลือกรับส่วนลด รับพิมพ์ 1 ไม่รับพิทพ์ 2 วน loop ถ้า input ไม่ใช่เลข 1,2
+        cout << "Ask for season discount." << endl;
+        cout << "Yes: 1" << " No: 2" << endl;
+        cout << "Answer: ";
+        cin >> decision;
+        cout << endl;
+    }
+        
+    if(decision == 1){
+
+        string season; //กำหนดฤดูจาก input เดือน: 1-4 = ฤดูร้อน, 5-8 = ฤดูฝน, 9-12 = ฤดูหนาว 
+        if(month == 1 || month == 2 || month == 3 || month == 4){
+            season = "Summer";
+        }else if(month == 5 || month == 6 || month == 7 || month == 8){
+            season = "Rainy";
+        }else if(month == 9 || month == 10 ||month == 11 ||month == 12){
+            season = "Winter";
+        }
+        
+        double discount; //สุ่มส่วนลดโดยหลังจากที่ input เลข 1 แล้วโอกาสที่จะได้ส่วนลดเป็น 30% ถ้าได้ส่วนลด จะสุ่มส่วนลด 1%-30% แล้วสรุปราคาส่วนลดก่อนและหลัง
+        srand(time(0));
+        int percent = rand()%10;
+        if(percent == 0 || percent == 1 || percent == 2){
+            discount = rand()%30;
+            cout << "You receive a " << discount+1 << "% discount from " << season << " promotion" << endl;
+            cout << "Total: " << price << " Baht" << endl;
+            cout << "Discount " << discount+1 << "%: " << (double) (price)*((discount+1)/100) << " Baht" << endl;
+            cout << "Total after discount: " << price - ((double) (price)*((discount+1)/100)) << " Baht";
+
+
+        }
+        else{ // กรณีที่สุ่มไม่ได้ส่วนลด โอกาส 70%
+            discount = 0;
+            cout << "Sorry, the promotion is unavailable now.";
+        }
+
+    
+    }else if(decision == 2){ //หลังจากที่ input เลข 2
+        cout << "You choose not to ask for season discount.";
+    }
+
+}    
+
