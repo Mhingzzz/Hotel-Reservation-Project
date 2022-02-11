89e5dbe8bd730403ed57ddcfd4e05944eefda5ea
ThankZ <natsuphat_th@cmu.ac.th>
2022-02-11 16:19:36 +0700

    completed

--- PrintReceipt.cpp
+++ PrintReceipt.cpp
@@ -1,4 +1,15 @@
 #include<iostream>
 using namespace std;
 
-//พิมพ์ใบเสร็จ
+int main(){
+    cout << "=============================================================" << endl;
+            cout << "                           Receipt                           " << endl;
+            cout << "Name --> " <<name << "Room ID --> "<< roomid << endl;
+            cout << "Day Start --> " << start1.datetext << endl;
+            cout << "Day End --> " << end1.datetext << endl;
+            cout << "Total Day --> " << totalday << endl;
+            cout << "Discount --> " << Discount << " On Season --> " << Season << endl;
+            cout << "Total Price : " << price - ((double) (price)*((Discount)/100)) << " Baht" << endl;
+            cout << "=============================================================" << endl;
+            return 0;
+}
