c1e983d4176d6b9b8fc93138563bc82837a298e5
640610645 <nontapan_chanadee@cmu.ac.th>
2022-02-15 19:22:22 +0700

    fix decision type

--- Discount.cpp
+++ Discount.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
-#include <ctime>
+#include<iostream>
+#include<string>
+#include<ctime>
 using namespace std;
 
 double Discount;
@@ -7,16 +8,22 @@ string Season;
 
 void RandomDiscount(int price,int month){ //function สุ่มส่วนลด รับ input เป็นราคารวมและเดือนที่จอง(วันแรก)
 
-    int decision; 
-    while(decision < 1 || decision > 2){ //ตัวเลือกรับส่วนลด รับพิมพ์ 1 ไม่รับพิทพ์ 2 วน loop ถ้า input ไม่ใช่เลข 1,2
-        cout << "Ask for season discount." << endl;
-        cout << "Accept: 1" << " Decline: 2" << endl;
-        cout << "Answer: ";
-        cin >> decision;
-        cout << endl;
+    string decision; 
+    while(decision != "1" || decision != "2"){ //ตัวเลือกรับส่วนลด รับพิมพ์ 1 ไม่รับพิทพ์ 2 วน loop ถ้า input ไม่ใช่เลข 1,2
+        cout << "==================================================================" << endl;
+        cout << "             Take a chance to get a season discount!" << endl;
+        cout << "         (30% chance of receiving a 1% to 30% discount)" << endl << endl;
+        cout << "                  Accept:[1] Decline:[2]" << endl;
+        cout << "==================================================================" << endl;
+        getline(cin,decision);
+        cout << "==================================================================" << endl << endl << endl;
+        if(decision == "1" || decision == "2") break;
+        else cout << "              Invalid input! Please enter again." << endl << endl << endl; 
     }
+
+    int d = stoi(decision);
         
-    if(decision == 1){
+    if(d == 1){
 
         string season; //กำหนดฤดูจาก input เดือน: 1-4 = ฤดูร้อน, 5-8 = ฤดูฝน, 9-12 = ฤดูหนาว 
         if(month == 1 || month == 2 || month == 3 || month == 4){
@@ -27,18 +34,18 @@ void RandomDiscount(int price,int month){ //function สุ่มส่วนล
             season = "Winter";
         }
         Season = season;
-        double discount; //สุ่มส่วนลดโดยหลังจากที่ input เลข 1 แล้วโอกาสที่จะได้ส่วนลดเป็น 30% ถ้าได้ส่วนลด จะสุ่มส่วนลด 1%-30% แล้วสรุปราคาส่วนลดก่อนและหลัง
+        double discount; //เสี่ยงโชคสุ่มส่วนลดโดยหลังจากที่ input เลข 1 แล้วโอกาสที่จะได้ส่วนลดเป็น 30% ถ้าได้ส่วนลด จะสุ่มส่วนลด 1%-30% แล้วสรุปราคาส่วนลดก่อนและหลัง
         srand(time(0));
         int percent = rand()%10;
         if(percent == 0 || percent == 1 || percent == 2){
             discount = rand()%30;
-            cout << "You receive a " << discount+1 << "% discount from " << season << " promotion" << endl;
+            cout << "==================================================================";
+            cout << "Congratulations! You receive a " << discount+1 << "% discount from " << season << " promotion" << endl;
             cout << "Total: " << price << " Baht" << endl;
             Discount = discount+1;
             cout << "Discount " << discount+1 << "%: " << (double) (price)*((discount+1)/100) << " Baht" << endl;
             cout << "Total after discount: " << price - ((double) (price)*((discount+1)/100)) << " Baht";
-
-
+            cout << "==================================================================";
         }
         else{ // กรณีที่สุ่มไม่ได้ส่วนลด โอกาส 70%
             discount = 0;
@@ -47,10 +54,14 @@ void RandomDiscount(int price,int month){ //function สุ่มส่วนล
         }
 
     
-    }else if(decision == 2){ //หลังจากที่ input เลข 2
-        cout << "You decline to ask for season discount.";
+    }else if(d == 2){ //หลังจากที่ input เลข 2
+        cout << "You do not get a season discount.";
         Discount = 0;
     }
 
 }    
 
+int main(){
+    RandomDiscount(1999,12);
+    return 0;
+}
